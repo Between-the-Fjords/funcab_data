4384945d57ed08739209aaeca66d5db3e43b2753
audhalbritter <me@audhalbritter.com>
2021-12-09 09:30:56 +0100

    clean recruitment data

--- R/recruitment/recruitment_data_processing.R
+++ R/recruitment/recruitment_data_processing.R
@@ -4,53 +4,35 @@
 
 # load libraries
 source("R/load_packages.R")
-library(DBI)
-library(RSQLite)
+
+# source TTC turfIDs
+source("R/community/dictionaries.R")
 
 # Download raw data
+get_file(node = node,
+         file = "FunCaB_raw_recruitment_2018-2019.csv",
+         path = "data/community",
+         remote_path = "3_Plant_composition")
+
 get_file(node = node,
          file = "FunCaB_clean_community_2015-2019.csv",
          path = "data/community",
          remote_path = "3_Plant_composition")
 
-# download seedclim database (control plots)
-get_file(node = "npfa9",
-         file = "seedclim.2020.4.15.zip",
-         path = "data/community",
-         remote_path = "3_Community_data")
-
-unzip("data/community/seedclim.2020.4.15.zip", exdir = "data/community")
-# !!! Move to raw folder !!!
-
-
-#source("~/Documents/FunCaB/figures/plotting_dim.R")
-
-#source("~/Documents/FunCaB/climate/LT_climate.R")
-
-#load seedling data
-#load("~/OneDrive - University of Bergen/Research/FunCaB/Data/secondary/composition_211123.RData")
-
-#gs4_deauth() # works for googlesheets where link sharing is ON
-#dat <- read_sheet(ss = "https://docs.google.com/spreadsheets/d/1eDVV6pAoeVfwaUGzdQuWrmN2_9s7q3EhbYQwbtvnP_M/edit#gid=595311948", sheet = "FUNCAB_recruitment2019", col_types = "c")
 
 # read in raw seedling data
-dat <- read_delim("data/recruitment/funcab_2019 - FUNCAB_recruitment2019.csv",
-                  col_types = )
+dat <- read_delim("data/recruitment/funcab_2019 - FUNCAB_recruitment2019.csv")
+
+# fix site names
+dat <- dat %>%
+  mutate(site = recode(site,
+                       "Ovstedal" = "Ovstedalen",
+                       "Skjellingahaugen" = "Skjelingahaugen",
+                       "Ulvhaugen" = "Ulvehaugen"))
 
 # read in community data
 comp2 <- read_delim(file = "data/community/FunCaB_clean_composition_2015-2019.csv")
 
-# create dataframe of all turfs
-all_turfs <- comp2 %>%
-  filter(treatment != "XC") %>%
-  distinct(site = siteID, turfID) %>%
-  crossing(year = seq(2018:2019),
-           round = seq(1:4)) %>%
-  filter(case_when(
-    year == 1 ~ round == 1:2,
-    year == 2 ~ round == 3:4)) %>%
-  mutate(year = case_when(year == 1 ~ 2018,
-                          year == 2 ~ 2019))
 
 # generate random seedling IDs for missing seedIDs
 recruitment <- dat %>%
@@ -58,7 +40,7 @@ recruitment <- dat %>%
          !Comment %in% c("out of plot"),
          !is.na(presence1) | !is.na(presence2) | !is.na(presence3) |!is.na(presence4)) %>%
   mutate(seedID = case_when(
-    is.na(seedID) ~ paste0("r", floor(runif(nrow(is.na(.)), min = 2000, max = 5000))),
+    is.na(seedID) ~ paste0("r", row_number(), "_", turfID),
     TRUE ~ seedID))
 
 # seedlings with duplicate IDs
@@ -72,129 +54,75 @@ recruitment %>%
 recruitment1 <- recruitment %>%
   mutate(seedID = paste0(seedID,"_", turfID),
          presence1 = as.character(presence1)) %>%
-  select(site, turfID, seedID, date1:date4, Obs1:Obs4, presence1:presence4) %>%
-  pivot_longer(date1:presence4,
-               names_to = c("category", "round"),
-               names_pattern = "(^.{0,8})([1-4])") %>%
-  pivot_wider(values_from = value, names_from = category) %>%
+  pivot_longer(cols = unID:Viola,
+               names_to = "species",
+               values_to = "temp_present", values_drop_na = TRUE) %>%
+  select(-temp_present) %>%
+  # no duplicates here
+  # group_by(site, blockID, treatment, turfID, seedID, species) %>%
+  # summarise(n = n()) %>% filter(n > 1)
+  pivot_longer(cols = matches("\\d"),
+               names_to = c(".value", "round"),
+               names_pattern = "(.*)(\\d$)") %>%
+  tidylog::filter(!is.na(presence)) %>%
+  # fixing one wrongly formatted date
+  mutate(date = if_else(date == "8/82019", "8/8/2019", date)) %>%
   mutate(date = dmy(date),
-         year = year(date),
-         round = as.numeric(as.factor(round))
+         year = if_else(round %in% c(1, 2), 2018, 2019),
+         round = as.numeric(round)
          ) %>%
-  full_join(all_turfs) %>%
+  select(year, date, siteID = site, blockID, plotID = turfID, treatment, seedID, round, species, presence, x, y, observer = Obs, Comment, Tttreat) %>%
   mutate(presence = case_when(
-    presence %in% c("NF", "not found", "NA") ~ "0",
+    presence %in% c("NF", "not found") ~ "0",
     presence %in% c("1?") ~"1",
+    #presence %in% c("225") ~"25",
     is.na(presence) ~ "0",
     TRUE ~ presence
   ),
   presence = as.numeric(presence))
 
 
-# make total seedling count per turf and per observation round
-mean_survival <- recruitment1 %>%
-  filter(presence < 2) %>%
-  group_by(turfID, seedID) %>%
-  mutate(seed_count = sum(presence)) %>%
-  group_by(turfID) %>%
-  summarise(mean_survival = mean(seed_count))
 
-multiples <- recruitment1 %>% filter(presence > 1)
-
+# fix species
 recruitment1 <- recruitment1 %>%
+  mutate(species = str_replace(species, "_", "."),
+         species = recode(species,
+                        "Anem.sp" = "Ane.sp",
+                        "Bellis" = "Bel.sp",
+                        "Betula" = "Bet.sp",
+                        "Epil.sp" = "Epi.sp",
+                        "Galium" = "Gal.sp",
+                        "Hier.sp" = "Hie.sp",
+                        "Leon.sp" = "Leo.sp",
+                        "Leuc.sp" = "Leu.sp",
+                        "Oma" = "Oma.sp",
+                        "Ranu.sp" = "Ran.sp",
+                        "Rume.sp" = "Rum.sp",
+                        "Stel.sp" = "Ste.sp",
+                        "Tara.sp" = "Tar.sp",
+                        "Trif.sp" = "Tri.sp",
+                        "unID" = "NID.seedling",
+                        "Veronica" = "Ver.sp",
+                        "Gal sax" = "Gal.sax",
+                        "Viola" = "Vio.sp"))
+
+# make multiple seedlings into separate observation and add unique seedID
+# check if this sum is the same as uncount
+#recruitment1 %>% filter(presence > 1) %>% summarise(sum(presence))
+new_seedlings19 <- uncount(data = recruitment1 %>%
+          filter(presence > 1), weights = presence) %>%
+  mutate(seedID = paste0("s", row_number(), "_", plotID))
+
+
+recruitment2 <- recruitment1 %>%
   filter(presence < 2) %>%
-  group_by(turfID, round) %>%
-  mutate(total_seedlings = sum(presence)) %>%
-  full_join(mean_survival, by = "turfID") %>%
-  bind_rows(multiples)
+  bind_rows(new_seedlings19) %>%
+  # fix turfID
+  left_join(dict_TTC_turf, by = c("plotID" = "turfID")) %>%
+  rename(turfID = TTtreat) %>%
+  select(-Tttreat) %>%
+  mutate(blockID = paste0(substr(siteID, 1, 3), blockID),
+         functional_group = "forb")
 
-save(recruitment1, file = "~/OneDrive - University of Bergen/research/FunCaB/Data/secondary/recruitment_cleaned.csv")
-
-# recruitment %>%
-#   filter(is.na(presence) | presence %in% c("1", "0")) %>%
-#   mutate(presence = as.numeric(presence),
-#          presence = coalesce(presence, 0)) %>%
-#   select(-date, -year, - Obs) %>%
-#   group_by(round, turfID) %>%
-#   summarise(tot = n(),
-#             totS = sum(presence)) %>%
-#   pivot_wider(names_from = round, values_from = presence, names_prefix = "round_")
-#             survival = (totS/tot)*100
-
-
-
-
-
-# join onto complete turf list to catch turfs with zero seedlings
-
-
-
-
-
-
-# load seed mass trait data
-con <- src_sqlite(path = "~/OneDrive - University of Bergen/Research/FunCaB/seedclim.sqlite", create = FALSE)
-
-
-
-######### data preparation ###########
-seedMass <- tbl(con, "numeric_traits") %>%
-  filter(trait == "seedMass") %>%
-  collect()
-
-turfDict <- comp2 %>%
-  distinct(siteID, blockID, Treatment, turfID) %>%
-  filter(!Treatment  == "XC") %>%
-  mutate(Round = 1, Round2 = 2) %>%
-  gather(Round, Round2, key = "v", value = "Round") %>%
-  select(-v)
-
-
-# merge composition data with seed mass data
-seedcomp <- comp2 %>%
-  filter(Year == 2018) %>%
-  left_join(seedMass) %>%
-  group_by(siteID, blockID, turfID, Treatment, mossCov, vegetationHeight, mossHeight, forbCov, graminoidCov, functionalGroup) %>%
-  summarise(seedMass = weighted.mean(value, cover)) %>%
-  ungroup()
-
-######### ------ abundance ------ ###########
-seedTot <- seed %>%
-  group_by(siteID, Round, blockID, Treatment, turfID) %>%
-  summarise(seed = sum(n())) %>%
-  ungroup() %>%
-  distinct(siteID, Round, blockID, Treatment, turfID, seed) %>%
-  right_join(turfDict %>% mutate(Round = as.factor(Round))) %>%
-  mutate(seed = case_when(is.na(seed) ~ 0L,
-                             TRUE ~ seed)) %>%
-  ungroup()
-
-seed2018 <- seedTot %>%
-  mutate(Treatment = recode(Treatment, "FGB" = "Gap", "C" = "Intact", "GF" = "B", "F" = "GB", "FB" = "G", "GB" = "F", "B" = "GF", "G" = "FB")) %>%
-  mutate(monthN = recode(Round, `1` = "spr", `2` = "aut"),
-         year = 2018) %>%
-  select(-Round)
-
-seedTot <- seed2018 %>%
-  filter(Treatment %in% c("Intact", "Gap"))
-
-
-######### ----- survival ----- ###########
-survival <- seed %>%
-  filter(Round == 1) %>%
-  select(siteID, blockID, Treatment, turfID, survival) %>%
-  mutate(survival = recode(survival, "dead" = 0, "alive" = 1, "not found" = 0, "1" = 1, "dezd" = 0)) %>%
-  group_by(Treatment, turfID, blockID, siteID) %>%
-  summarise(tot = n(),
-            totS = sum(survival),
-            survival = (totS/tot)*100) %>%
-  right_join(turfDict %>% mutate(Round = as.factor(Round))) %>%
-  mutate_at(tot = case_when(is.na(tot) ~ 0L,
-                             TRUE ~ tot)) %>%
-  mutate(totS = case_when(is.na(totS) ~ 0,
-                             TRUE ~ totS)) %>%
-  mutate(survival = case_when(is.na(survival) ~ 0,
-                             TRUE ~ survival)) %>%
-  left_join(weather) %>%
-  ungroup()
+write_csv(recruitment2, file = "data/recruitment/FunCaB_clean_recruitment_2018-2019.csv")
 
